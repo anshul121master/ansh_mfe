af1d8461b6e5fddd2f4da17f8391f9df00ca695d
Anshul Agarwal <anshula128@gmail.com>
2021-01-29 13:52:50 +0530

    first commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,24 @@
+import React, { useRef, useEffect } from 'react'
+import { mount } from 'marketing/MarketingApp'
+
+// export default class Marketing extends React.Component{
+//    constructor(props) {
+//     super(props);
+//     this.myRef = React.createRef();
+//   }
+//   componentDidMount(){
+//       mount(this.myRef.current);
+//   }
+//   render() {
+//     return <div id="container" ref={this.myRef} />;
+//   }
+// }
+
+export default () =>{
+    const ref = useRef(null);
+    useEffect(() =>{
+        mount(ref.current)
+    })
+
+    return <div ref={ref}></div>
+}
