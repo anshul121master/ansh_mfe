978e6e5db8aeae9887bcf4199b4eaf307a0a6dc5
Anshul Agarwal <anshula128@gmail.com>
2021-01-29 16:39:01 +0530

    Created script for container flow

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json')
 
 const devConfig = {
     mode:'development',
@@ -18,10 +18,7 @@ const devConfig = {
                     marketing: 'marketing@http://localhost:8081/remoteEntry.js',
                    // cart: 'cart@http://localhost:8082/remoteEntry.js'
                 },
-                shared:['react', 'react-dom']
-            }),
-            new HtmlWebpackPlugin({
-                template:'./public/index.html'
+                shared: packageJson.dependencies
             })
         ]
     }
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json')
 
 const devConfig = {
     mode:'development',
@@ -18,10 +18,7 @@ const devConfig = {
                 exposes: {
                     './MarketingApp': './src/bootstrap'
                 },
-                shared:['react', 'react-dom']
-            }),
-            new HtmlWebpackPlugin({
-                template:'./public/index.html'
+                shared: packageJson.dependencies
             })
         ]
     }
