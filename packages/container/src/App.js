2ec267e488a9fa482c47d951183157188b25f078
Anshul Agarwal <anshula128@gmail.com>
2021-02-01 10:58:06 +0530

    removed chrislenon aws cli installation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 export default () =>{
     return(
         <React.Fragment>
-        <h1>This is Marketing App From Container. Enjoy now its working</h1>
+        <h1>This is Marketing App From Container. Enjoy now its working!!</h1>
+        <Header />
         <MarketingApp />
         </React.Fragment>
     )
